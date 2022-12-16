77905aaf288c30b3711f6a56f68a3ab2e1ea23e5
agerada <sandrogerada@gmail.com>
2022-12-16 20:14:59 +0000

    created test

--- src/R/pull_PATRIC_esco.R
+++ src/R/pull_PATRIC_esco.R
@@ -5,8 +5,12 @@ library(Biostrings)
 library(Rcpp)
 library(bench)
 
+# directories
+database_txt_dir <- "data/databases/patric/PATRIC_genomes_AMR.txt"
+genomes_download_dir <- "data/genomes/patric/"
+
 # make list of unique genome_id for download
-patric_amr_list <- read_delim('public_data/PATRIC_genomes_AMR.txt', delim='\t')
+patric_amr_list <- read_delim(database_txt_dir, delim='\t')
 
 # select only samples with broth or agar dilution
 patric_amr_list_esco_agar_broth <- patric_amr_list %>% 
@@ -15,8 +19,9 @@ patric_amr_list_esco_agar_broth <- patric_amr_list %>%
 
 esco_genome_ids <- unique(patric_amr_list_esco_agar_broth$genome_id)
 
+# set download dir
 old_wd <- getwd()
-setwd("public_data/genomes")
+setwd(genomes_download_dir)
 
 n_downloads <- length(esco_genome_ids)
 # check we are not trying to downloading more than available
@@ -36,53 +41,3 @@ while(i <= n_downloads){
   i <- i+1
 }
 setwd(old_wd)
-
-
-
-seq1 <- readDNAStringSet(filepath = "public_data/genomes/562.101663.fna")
-seq2 <- readDNAStringSet(filepath = "public_data/SAMEA2204418.contigs.fa.gz.filtered.fa")
-
-sourceCpp('kmer.cpp')
-kmers(s)
-s <- as.character(seq1[[1]])
-bench::mark(
-kmers(as.character(unlist(seq1)), kmer=10), iterations = 1
-)
-read_kmer_r(s, kmer=10)
-
-kmers(as.character(unlist(seq1)), kmer=10)
-bench::mark(
-  kmers_pointed(s, 10, anchor = T), 
-  kmers_pointed(s, 10, anchor = F), 
-  kmers(s, 10), 
-iterations=10, 
-check=F
-)
-
-
-mclapply(list(as.character(unlist(seq1)),
-              as.character(unlist(seq2))), function(x) kmers(x, kmer=10), 
-         mc.cores = 4)
-
-# make kmer data
-sequences_filenames <- list.files(path="public_data/genomes", pattern = "*.fna", 
-                                  full.names = TRUE)
-# try 50 sequences first
-test_kmers <- mclapply(sequences_filenames[1:4], 
-         function(x) {
-           seq <- Biostrings::readDNAStringSet(x)
-           seq <- as.character(unlist(seq))
-           kmers(seq, kmer=10)
-         })
-
-# try new function
-bench::mark(
-  mclapply(sequences_filenames[1:4], 
-                     function(x) {
-                       seq <- Biostrings::readDNAStringSet(x)
-                       seq <- as.character(unlist(seq))
-                       kmers_pointed(seq, kmer=10, anchor=T)[[2]]
-                     }), 
-  memory=FALSE, 
-  iterations = 1
-)
