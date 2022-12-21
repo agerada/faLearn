5819074c03e4635686d51dab28537175a92d7f98
agerada <sandrogerada@gmail.com>
2022-12-21 16:13:12 +0000

    added script to install R dependencies

---
+++ src/R/install_r_dependencies.R
@@ -0,0 +1,24 @@
+#!/usr/bin/env Rscript
+# Copyright 2022 Alessandro Gerada alessandro.gerada@liverpool.ac.uk
+
+packages <- c("tidyverse", "parallel", "glue", "Rcpp", 
+              "bench", "snow", "pbapply")
+installed <- installed.packages()
+
+if(!("BiocManager" %in% installed)) install.packages("BiocManager")
+# install core Bioconductor
+message("Installing/updating Bioconductor")
+BiocManager::install()
+# install Biostrings
+message("Installing Biostrings")
+BiocManager::install("Biostrings")
+
+install.packages(setdiff(packages, installed))
+
+# test Rcpp
+message("Testing Rcpp")
+if (Rcpp::evalCpp("2+2") == 4) {
+  message("Rcpp functioning correctly.")
+} else {
+  stop("Rcpp not functioning. Check installation and C++ compiler. ")
+}
