663a6d34143697cb1668223830dc7221e087e6b8
agerada <sandrogerada@gmail.com>
2023-06-13 20:04:53 +0100

    wrapped and working

--- src/R/boost.R
+++ src/R/boost.R
@@ -5,6 +5,8 @@ library(xgboost)
 
 parallel <- TRUE
 antibiotic_subset <- c("ciprofloxacin", "gentamicin", "ampicillin")
+antibiotic_process_restrict <- c("ciprofloxacin", "gentamicin", "ampicillin")
+model_type <- "binary"
 
 clean_raw_mic <- function(mic) {
   # remove leading equals sign
@@ -17,6 +19,13 @@ clean_raw_mic <- function(mic) {
 database_path <- "data/databases/patric/PATRIC_genomes_AMR.txt"
 kmers_path <- "data/output/projections/6_kmer_backup.csv"
 database <- read_delim(database_path, col_types = cols(.default = "c"))
+
+if(!is.null(antibiotic_process_restrict)){
+  antibiotic_names <- antibiotic_process_restrict
+} else {
+  antibiotic_names <- unique(database$antibiotic)
+}
+
 kmers <- read_csv(kmers_path,
                   col_types = cols(V1 = col_character())) 
 
@@ -41,7 +50,6 @@ make_meta_data <- function(patric_meta_data,
   meta_data <- inner_join(database_filtered, meta_data) %>%
     filter(measurement_unit == measurement_unit_filter) 
   
-  antibiotic_names <- unique(meta_data$antibiotic)
   meta_data <- meta_data %>% 
     pivot_wider(id_cols = genome_id, 
                 names_from = antibiotic, 
@@ -49,44 +57,62 @@ make_meta_data <- function(patric_meta_data,
     left_join(distinct(patric_meta_data, genome_id, .keep_all = T))
   
   meta_data <- meta_data %>% 
-    mutate(across(all_of(antibiotic_names), clean_raw_mic))
+    mutate(across(any_of(antibiotic_names), clean_raw_mic))
 }
 
 meta_data <- make_meta_data(database, kmers)
-meta_data <- meta_data %>% select(all_of(c('genome_id', 'genome_name', antibiotic_subset)))
+meta_data <- meta_data %>% select(any_of(c('genome_id', 'genome_name', antibiotic_names)))
 
 # process MICs
-if(parallel) {
-  cores <- parallel::detectCores()
-  cluster <- multidplyr::new_cluster(cores)
-  cluster_copy(cluster, c("antibiotic_subset", "clean_raw_mic"))
-  system.time(
-    meta_data <- meta_data %>% 
-      partition(cluster) %>% 
-      mutate(across(all_of(antibiotic_subset), clean_raw_mic)) %>% 
-      mutate(across(all_of(antibiotic_subset), AMR::as.mic)) %>% 
-      collect()
-  )
-} else {
-  system.time(
-    meta_data <- meta_data %>% 
-      mutate(across(all_of(antibiotic_subset), AMR::as.mic))
-  )
+clean_up_mics <- function(data, abx_names, parallel = TRUE) {
+  if(parallel) {
+    cores <- parallel::detectCores()
+    cluster <- multidplyr::new_cluster(cores)
+    cluster_copy(cluster, c("abx_names", "clean_raw_mic"))
+    system.time(
+      meta_data <- meta_data %>% 
+        partition(cluster) %>% 
+        mutate(across(any_of(abx_names), clean_raw_mic)) %>% 
+        mutate(across(any_of(abx_names), AMR::as.mic)) %>% 
+        collect()
+    )
+  } else {
+    system.time(
+      meta_data <- meta_data %>% 
+        mutate(across(all_of(abx_names))) %>% 
+        mutate(across(all_of(abx_names), AMR::as.mic))
+    )
+  }
+  return(meta_data)
 }
 
-# convert to binary
-if(parallel) {
-  cores <- parallel::detectCores()
-  cluster <- multidplyr::new_cluster(cores)
-  cluster_copy(cluster, c("antibiotic_subset"))
-  
-  system.time(
-    meta_data_binary <- meta_data %>% 
-      partition(cluster) %>% 
-      mutate(organism_name = AMR::as.mo(genome_name)) %>% 
-      mutate(across(all_of(antibiotic_subset), AMR::as.sir)) %>% 
-      collect()
-  )
+convert_mic_to_sir <- function(data, abx_names, parallel = TRUE) {
+  if(parallel) {
+    cores <- parallel::detectCores()
+    cluster <- multidplyr::new_cluster(cores)
+    cluster_copy(cluster, c("abx_names"))
+    
+    system.time(
+      meta_data_binary <- data %>% 
+        partition(cluster) %>% 
+        mutate(organism_name = AMR::as.mo(genome_name)) %>% 
+        mutate(across(any_of(abx_names), AMR::as.sir)) %>% 
+        collect()
+    )
+  } else {
+    system.time(
+      meta_data_binary <- data %>% 
+        mutate(organism_name = AMR::as.mo(genome_name)) %>% 
+        mutate(across(any_of(abx_names), AMR::as.sir))
+    )
+  }
+  return(meta_data_binary)
+}
+
+meta_data_clean_mics <- clean_up_mics(meta_data, antibiotic_names)
+
+if(model_type == "binary") {
+  meta_data_sir <- convert_mic_to_sir(meta_data_clean_mics, antibiotic_names)
 }
 
 combine_kmer_metadata_and_split <- function(meta_data, 
@@ -116,7 +142,7 @@ combine_kmer_metadata_and_split <- function(meta_data,
               'test' = test_ds))
 }
 
-combined_data <- combine_kmer_metadata_and_split(meta_data_binary,
+combined_data <- combine_kmer_metadata_and_split(meta_data_sir,
                                                  kmers, 
                                                  ciprofloxacin)
 training_data <- combined_data[['train']]
@@ -124,16 +150,17 @@ testing_data <- combined_data[['test']]
 
 # start with just CIP
 make_xgmodel_binary <- function(training_data, iterations = 100, 
-                         train_frac = 0.8){ 
+                         train_frac = 0.8, cores){ 
   bstDMatrix <- xgboost(data = training_data, max.depth = 2, eta = 1, nthread = cores, 
                         nrounds = iterations, objective = "binary:logistic")
 }
-  
-xgmodel <- make_xgmodel_binary(training_data)
+
+
+xgmodel <- make_xgmodel_binary(training_data, cores = 4)
 
 accuracy_binary <- function(xg_model, test_dataset){
   labels <- getinfo(test_dataset, 'label')
-  return(sum(as.numeric(predict(xg_model, test_dataset) > 0.5) == labels) / nrow(test_ds))
+  return(sum(as.numeric(predict(xg_model, test_dataset) > 0.5) == labels) / nrow(test_dataset))
 }
 
 accuracy_binary(xgmodel, testing_data)
