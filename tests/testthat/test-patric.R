067563afc0b1db63c35d791a7c6c096fcfe15c4a
Alessandro Gerada <62601884+agerada@users.noreply.github.com>
2025-01-28 23:14:59 +0000

    cran skip

--- tests/testthat/test-patric.R
+++ tests/testthat/test-patric.R
@@ -7,12 +7,15 @@ test_db <- data.frame(genome_id = 1,
                       laboratory_typing_method = "test",
                       resistant_phenotype = "test")
 
-test_that("multiplication works", {
+test_that("local load patric works", {
   expect_s3_class(load_patric_db(test_patric_path), "patric_db")
-  expect_s3_class(load_patric_db(), "patric_db")
-
   db_df <- read.delim(test_patric_path)
   expect_s3_class(as_patric_db(db_df), "patric_db")
+})
+
+test_that("load patric from ftp works", {
+  skip_on_cran()
+  expect_s3_class(load_patric_db(), "patric_db")
 })
 
 test_that("check valid patric_db works",{
@@ -26,12 +29,14 @@ test_that("check valid patric_db works",{
 })
 
 test_that("check can save patric db", {
+  skip_on_cran()
   tmp_path_db <- tempfile(fileext = ".txt")
   save_patric_db(tmp_path_db)
   expect_s3_class(load_patric_db(tmp_path_db), "patric_db")
 })
 
 test_that("check can download genomes", {
+  skip_on_cran()
   tmp_dir <- tempdir()
   unlink(list.files(tmp_dir, full.names = TRUE, pattern = "*.fna"))
   pull_PATRIC_genomes(tmp_dir,
